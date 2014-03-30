a77c2cc4cd7a78c19e7a04d5aa4fef66d27a577a
frankcash <cfcash@mtu.edu>
2014-03-30 11:05:25 -0400

    Should fix the memory issue.

--- demos/example_with_web-server.js
+++ demos/example_with_web-server.js
@@ -7,43 +7,47 @@ var Steam = require('steam-webapi');
 var express = require('express');
 
 // sets your key to var
-var steamAPIKey = "<key>";
+var steamAPIKey = "84E02F1B45E5C2655871C427A03D9883";
 
 var app = express(); // creates server
-var metadataArray = []; // initializes the array
-function getInfo(callback){
-  Steam.ready(steamAPIKey, function(err) {
-      if (err) return console.log(err);
 
-      var steam = new Steam({key: steamAPIKey});
+Steam.ready(steamAPIKey, function(err) {
+    if (err) return console.log(err);
 
-      // Retrieve the steam ID from a steam username/communityID
-      steam.resolveVanityURL({vanityurl:'<steam_id>'}, function(err, data) {
-          // data -> { steamid: '76561197968620915', success: 1 }
-          console.log(data);
+    var steam = new Steam({key: steamAPIKey});
 
-          // sets the game ID to Dota2
-          data.gameid = Steam.DOTA2;
+    // Retrieve the steam ID from a steam username/communityID
+    steam.resolveVanityURL({vanityurl:'howdoiskill'}, function(err, data) {
+        // data -> { steamid: '76561197968620915', success: 1 }
+        var metadataArray = []; // initializes the array
+        console.log(data);
 
-          // gets heroes from Dota2
-          steam.getHeroes(data, function (err, data) {
-              console.log(data);  // {heroes: [ {name: 'npc_dota_hero_antimage', id:1} ... ]}
+        // sets the game ID to Dota2
+        data.gameid = Steam.DOTA2;
 
-              // this array will be manipulatable by calling an index and then a property
-              metadataArray.push(data); // sends it to an array so the JSON can be displayed on the page/used easier
+        // gets heroes from Dota2
+        steam.getHeroes(data, function (err, data) {
+            console.log(data);  // {heroes: [ {name: 'npc_dota_hero_antimage', id:1} ... ]}
 
-              callback(metadataArray); // adds a callback
-          });
-      });
-  });
-}
+            // this array will be manipulatable by calling an index and then a property
+            metadataArray.push(data); // sends it to an array so the JSON can be displayed on the page/used easier
 
-app.get('/', function(req, res){
-  // res.send(JSON.stringify(metadataArray, null, 4)); // sends this to the server
-  getInfo(function(data){
-    res.send(data) // sends the data to the server
-  });
-});
+
+        }); // end of steam.getHeroes
+
+        app.get('/', function(req, res){
+          res.send(JSON.stringify(metadataArray, null, 4)); // sends this to the server
+        }); // end of app.get
+
+    }); // end of steam.resolveVanity URL
+
+
+
+}); // end of steam.ready
+
+
+
+
 
 
 app.listen(8080); // listens to port 8080
